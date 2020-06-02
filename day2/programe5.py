59938ec25a56cb272a35161c9c1bede0f6f5f93f
NukalaVenkataSupriya <66204717+nukalavenkatasupriya@users.noreply.github.com>
2020-06-02 23:21:33 +0530

    Create programe5.py

---
+++ day2/programe5.py
@@ -0,0 +1,5 @@
+N = int(input("Enter the Value of N: "))
+for i in range(N):
+    print((str(N - i) + "*") * (N - 1 - i) + str(N - i))
+for i in range(2,N + 1):
+    print((str(i) + "*") * (i - 1) + str(i))
